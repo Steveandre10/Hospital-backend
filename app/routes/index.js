777b8e40f430c91dd502a845e9d5abb1b5089064
Steven Vergara <mavil10122225@gmail.com>
2026-07-04 19:32:57 -0500

    Enlace directo con los puertos de la BD

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,9 @@ import paciente from './paciente.routes.js';
 
 const route = Router();
 
+route.get('/', (req, res) => {
+    res.send('Bienvenido a la API de Citas Médicas');
+});
 route.use('/citas', cita);
 route.use('/pacientes', paciente);
 
